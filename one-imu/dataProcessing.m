ea0a0c42c290eff7d01f479cc0ad73a0b5694f05
AJ Bull <augustb@umich.edu>
2022-04-05 13:45:48 -0400

    Removed improper angular-to-linear velocity compensation attempt

--- one-imu/dataProcessing.m
+++ one-imu/dataProcessing.m
@@ -73,10 +73,6 @@ for i = 2:3068  % 3068 is number of timesteps for which we have IMU
     inputs = [inputs(2);-inputs(1);inputs(3);...
             inputs(5);-inputs(4);inputs(6)];
     % compensate linear velocity w/ displacement of IMU
-    % This line is sus, am I doing it right?
-    inputs(4:6) = inputs(4:6) + skew3x3(inputs(1:3))*imu1_p;
-    % Check with and without this
-    % inputs(1:3) = inputs(1:3)*pi/180; 
     [X,P] = predict(inputs, dt, X, P, A, Q);
     if sum(isnan(P(:))) > 0
         warning('Detected NaN in P')
